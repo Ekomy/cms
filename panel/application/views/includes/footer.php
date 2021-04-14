a3a77e5fc7d8340be71f154c12d6d54f0e32709f
Ekomy <darendeli.ekin@gmail.com>
2021-04-15 00:54:22 +0200

    dashboard_v için bir klasör oluşturduk. Boş content sayfası yapıldı.

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,8 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+
+            <div class="copyright pull-left">Tüm hakları saklıdır | edarendeli <?php echo date("Y"); ?>) 2021 &copy;</div>
+        </div>
+    </footer>
+</div>

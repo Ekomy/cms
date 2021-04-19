1c3a012743d37fe4840cb9df5905b5cb75fb8a12
Ekomy <darendeli.ekin@gmail.com>
2021-04-19 17:36:16 +0200

    sweetalert entegresi yapildi. custom.js dosyasi eklendi.

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    swal({
+        title: 'Error!',
+        text:'Do you want to continue',
+        type:'error',
+        confirmButtonText: 'Cool'
+    })
+
+    }
+
+
+)

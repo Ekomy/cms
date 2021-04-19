a4d8e00d4cef5dd39d2d2f2c1431361a2ab753ef
Ekomy <darendeli.ekin@gmail.com>
2021-04-19 18:08:00 +0200

    SweetAlert ile confirm kullanılarak Kayıt ve Silme islemi gerceklestirildi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -44,7 +44,11 @@
                                     />
                                 </td>
                                 <td>
-                                    <a href="<?php echo base_url("product/delete/$item->id"); ?>" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                                    <button
+                                            data-url="<?php echo base_url("product/delete/$item->id"); ?>"
+                                            class="btn btn-sm btn-danger btn-outline remove-btn">
+                                            <i class="fa fa-trash"></i> Sil
+                                    </button>
                                     <a href="<?php echo base_url("product/update_form/$item->id"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
                                 </td>
                             </tr>
--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 
-    swal({
-        title: 'Error!',
-        text:'Do you want to continue',
-        type:'error',
-        confirmButtonText: 'Cool'
+    $('.remove-btn').click(function (e){
+
+        $data_url = $(this).data("url");
+        
+        swal({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then(function (result){
+            if (result.value) {
+                window.location.href = $data_url;
+            }
+        });
     })
 
-    }
-
-
-)
+    })
